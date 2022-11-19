0e0a39ac60ec9bcbcff496e61e31420466835c30
Mary Ball <marusaball@yandex.ru>
2022-11-19 16:59:31 +0300

    Проектная работа 10(1)

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,32 @@
-function Card ({card, onCardClick}) {
-	const handleClick = () => {
-		onCardClick(card)
-	}
-	return (
-        <article className="element">
-                <button className="element__delete" aria-label="Delete" type="button"></button>
-                <img src={card.link} alt={card.name} onClick={() => handleClick()} className="element__image"/>
-                <div className="element__container">
-                    <p className="element__title">{card.name}</p>
-                    <div className="element__container-like">
-                        <button className="element__heart" aria-label="Like" type="button"></button>
-                        <div className="element__like">{card.likes.length}</div>
-                    </div>
-                </div>
-            </article>
-    )
-
-    }
+function Card({ card, onCardClick }) {
+  const handleClick = () => {
+    onCardClick(card);
+  };
+  return (
+    <article className="element">
+      <button
+        className="element__delete"
+        aria-label="Delete"
+        type="button"
+      ></button>
+      <img
+        src={card.link}
+        alt={card.name}
+        onClick={() => handleClick()}
+        className="element__image"
+      />
+      <div className="element__container">
+        <p className="element__title">{card.name}</p>
+        <div className="element__container-like">
+          <button
+            className="element__heart"
+            aria-label="Like"
+            type="button"
+          ></button>
+          <div className="element__like">{card.likes.length}</div>
+        </div>
+      </div>
+    </article>
+  );
+}
 export default Card;
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,100 +1,86 @@
 class Api {
-    constructor({baseURL, headers}) {
-        this.url = baseURL;
-        this._headers = headers;
+  constructor({ baseURL, headers }) {
+    this.url = baseURL;
+    this._headers = headers;
+  }
+
+  _checkResponse(response) {
+    if (response.ok) {
+      return response.json();
     }
+    return Promise.reject(`Ошибка: ${response.status}`);
+  }
 
-    _checkResponse(response) {
-        if (response.ok) {
-          return response.json();
-        };
-        return Promise.reject(`Ошибка: ${response.status}`);
-      };
+  //отображение данных профиля с сервера
+  getUserInfo() {
+    return fetch(`${this.url}/users/me`, {
+      method: "GET",
+      headers: this._headers,
+    }).then(this._checkResponse);
+  }
 
-    //отображение данных профиля с сервера
-    getUserInfo() {
-        return fetch(`${this.url}/users/me`, {
-                method: "GET",
-                headers: this._headers,
-            })
-            .then(this._checkResponse);
-        };
+  editUserInfo(data) {
+    return fetch(`${this.url}/users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify(data),
+    }).then(this._checkResponse);
+  }
 
-    editUserInfo(data) {
-         return fetch(`${this.url}/users/me`, {
-            method: 'PATCH',
-            headers: this._headers,
-            body: JSON.stringify(data),
-            })
-           .then(this._checkResponse);
-    }
+  //отображение карточек с сервера
+  getCards() {
+    return fetch(`${this.url}/cards`, {
+      method: "GET",
+      headers: this._headers,
+    }).then(this._checkResponse);
+  }
 
-    //отображение карточек с сервера
-    getCards() {
-        return fetch(`${this.url}/cards`, {
-                method: "GET",
-                headers: this._headers,
-            })
-            .then(this._checkResponse);
-    }
+  addNewCard(data) {
+    return fetch(`${this.url}/cards`, {
+      method: "POST",
+      headers: this._headers,
+      body: JSON.stringify(data),
+    }).then(this._checkResponse);
+  }
 
+  deleteCard(cardId) {
+    return fetch(`${this.url}/cards/${cardId}`, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then(this._checkResponse);
+  }
 
-    addNewCard(data) {
-        return fetch(`${this.url}/cards`, {
-                method: 'POST',
-                headers: this._headers,
-                body: JSON.stringify(data),
-                })
-                .then(this._checkResponse);
-    }
+  likeCard(cardId) {
+    return fetch(`${this.url}/cards/${cardId}/likes`, {
+      method: "PUT",
+      headers: this._headers,
+    }).then(this._checkResponse);
+  }
 
-    deleteCard(cardId) {
-        return fetch(`${this.url}/cards/${cardId}`, {
-                method: 'DELETE',
-                headers: this._headers,
-                })
-                .then(this._checkResponse);
-    }
+  removeLikeCard(cardId) {
+    return fetch(`${this.url}/cards/${cardId}/likes`, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then(this._checkResponse);
+  }
 
-    likeCard(cardId) {
-        return fetch(`${this.url}/cards/${cardId}/likes`, {
-                 method: 'PUT',
-                 headers: this._headers,
-                })
-                .then(this._checkResponse);
-    }
-
-    removeLikeCard(cardId) {
-        return fetch(`${this.url}/cards/${cardId}/likes`, {
-            method: 'DELETE',
-            headers: this._headers,
-           })
-           .then(this._checkResponse);
-    }
-
-     editAvatar(avatar) {
-        return fetch(`${this.url}/users/me/avatar`, {
-            method: 'PATCH',
-            headers: this._headers,
-            body: JSON.stringify({
-            avatar: avatar,
-        }),
-            })
-            .then(this._checkResponse);
-    }
-};
+  editAvatar(avatar) {
+    return fetch(`${this.url}/users/me/avatar`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        avatar: avatar,
+      }),
+    }).then(this._checkResponse);
+  }
+}
 
 const api = new Api({
-    baseURL: 'https://mesto.nomoreparties.co/v1/cohort-52',
-    headers: {
-        authorization: '0052e1a2-cce0-4efd-a4a0-a96b7082b662',
-        'Content-Type': 'application/json'
-    }
+  baseURL: "https://mesto.nomoreparties.co/v1/cohort-52",
+  headers: {
+    authorization: "0052e1a2-cce0-4efd-a4a0-a96b7082b662",
+    "Content-Type": "application/json",
+  },
 });
 
-
 export default api;
-
-
-
-
